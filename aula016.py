1a9eb0189bb864a32a0213f4aba16b4e6c2b46ef
João Victor Ferreira Barbosa <112912162+joao-victor-barbosa@users.noreply.github.com>
2024-01-20 10:42:02 -0300

    Add files via upload

---
+++ aula016.py
@@ -0,0 +1,14 @@
+# if / elif      / else
+# se/ se não se / se não
+
+acai = input('Você gosta de açaí, sim ou não? ')
+
+if acai == 'sim':
+    print('Você tem muito bom gosto!')
+    print('Parabens =)')
+elif acai == 'não':
+    print('Você é besta!')
+    print('VAI COMPRAR AÇAÍ')
+else:
+    print('Vc não digitou sim ou não')
+    print('ou vc esqueceu de colocar esse assento = ~')
